8d25ab8efa2976740dfb1ef607dcafa754e13115
lhl <lhl@randomfoo.net>
2013-05-09 17:20:21 -0700

    working on logging...

--- pensieve.py
+++ pensieve.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 
+import logging
 import time
 import os
+import sys
 import tornado.ioloop
 import tornado.web
 
 import handlers
 
 PORT = 1223
+
+logging.basicConfig(filename='/lensley/hype/print.log',
+                                        level=logging.DEBUG,
+                                        format = '%(asctime)s : %(levelname)s : %(message)s',
+                                        datefmt = '%Y-%m-%d %H:%M:%S'
+                                       )
+
+# LOGGING 
+
+if sys.stdout.isatty():
+  debug = 1
+else:
+  debug = 0
 
 class Application(tornado.web.Application):
   def __init__(self):
@@ -44,5 +59,7 @@ class MainHandler(tornado.web.RequestHandler):
       open(dest, 'w').write(content)
 
 if __name__ == '__main__':
+  if debug:
+    print "Starting on locahost:%s" % PORT
   Application().listen(PORT)
   tornado.ioloop.IOLoop.instance().start()
