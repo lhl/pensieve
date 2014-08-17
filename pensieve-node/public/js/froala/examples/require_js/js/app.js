d0224eb63707437a2f4ef09ac5ee8d82a1e01025
Leonard Lin <lhl@randomfoo.net>
2014-08-17 03:18:32 +0200

    removed js

--- pensieve-node/public/js/froala/examples/require_js/js/app.js
+++
@@ -1,6 +0,0 @@
-define(["jquery", "beautify-html", "froala_editor"], function($) {
-    $(function() {
-      html_beautify = require("beautify-html").html_beautify;
-      $('#edit').editable({inlineMode: false})
-    });
-});
