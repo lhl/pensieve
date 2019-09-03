527d9e5eda8e77b158201ef0cdd9c03980108f77
lhl <lhl@randomfoo.net>
2019-09-03 11:35:01 -0700

    Updated

--- sync/reddit/get-messages.py
+++ sync/reddit/get-messages.py
@@ -7,6 +7,11 @@ import praw
 import sys
 import time
 
+'''
+Reddit API status page: https://reddit.statuspage.io/
+Discussion: https://www.reddit.com/r/redditdev/
+'''
+
 creds = json.load(open('reddit.creds'))
 client_id = creds['client_id']
 client_secret = creds['client_secret']
@@ -24,6 +29,8 @@ def main():
   # print(reddit.auth.scopes())
   # listing = reddit.redditor('randomfoo2').comments.new()
   listing = reddit.user.me().comments.new(limit=None)
+  # listing = reddit.user.me().comments.hot(limit=None)
+  # listing = reddit.user.me().comments.controversial(limit=None)
 
   for l in listing:
     # If file exists, break
@@ -31,40 +38,43 @@ def main():
       print('Skipping comment {}...'.format(l))
       continue
 
-    # Create New File
-    print('Writing comment {}...'.format(l))
 
     # https://praw.readthedocs.io/en/latest/code_overview/models/comment.html?highlight=comment#praw.models.Comment
-    msg = {}
-    comment = reddit.comment(id=l)
-    msg['author_name'] = comment.author.name
-    msg['body'] = comment.body
-    msg['created_utc'] = comment.created_utc
-    msg['distinguished'] = comment.distinguished
-    msg['edited'] = comment.edited
-    msg['id'] = comment.id
-    msg['is_submitter'] = comment.is_submitter
-    msg['link_id'] = comment.link_id
-    msg['parent_id'] = comment.parent_id
-    msg['permalink'] = comment.permalink
-    msg['score'] = comment.score
-    msg['stickied'] = comment.stickied
-    msg['submission_id'] = comment.submission.id
-    msg['submission_permalink'] = comment.submission.permalink
-    msg['subreddit_display_name'] = comment.subreddit.display_name
-    msg['subreddit_name'] = comment.subreddit.name
-    msg['subreddit_id'] = comment.subreddit.id
+    try:
+      msg = {}
+      comment = reddit.comment(id=l)
+      msg['author_name'] = comment.author.name
+      msg['body'] = comment.body
+      msg['created_utc'] = comment.created_utc
+      msg['distinguished'] = comment.distinguished
+      msg['edited'] = comment.edited
+      msg['id'] = comment.id
+      msg['is_submitter'] = comment.is_submitter
+      msg['link_id'] = comment.link_id
+      msg['parent_id'] = comment.parent_id
+      msg['permalink'] = comment.permalink
+      msg['score'] = comment.score
+      msg['stickied'] = comment.stickied
+      msg['submission_id'] = comment.submission.id
+      msg['submission_permalink'] = comment.submission.permalink
+      msg['subreddit_display_name'] = comment.subreddit.display_name
+      msg['subreddit_name'] = comment.subreddit.name
+      msg['subreddit_id'] = comment.subreddit.id
 
-    with open('comments/{}.json'.format(l), 'w') as msg_outfile:
-      json.dump(msg, msg_outfile)
+      with open('comments/{}.json'.format(l), 'w') as msg_outfile:
+        print('Writing comment {}...'.format(l))
+        json.dump(msg, msg_outfile)
 
-    # comment.replies # CommentForest
-    # comment.submission # Submission
-    # comment.subreddit # Subreddit
+      # comment.replies # CommentForest
+      # comment.submission # Submission
+      # comment.subreddit # Subreddit
 
-    # sys.exit()
+      # sys.exit()
 
-    time.sleep(1)
+      time.sleep(1)
+    except:
+      print('ERROR comment {}...'.format(l))
+
 
 
 if __name__ == "__main__":
